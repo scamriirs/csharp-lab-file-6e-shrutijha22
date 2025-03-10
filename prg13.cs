5faa062767375bbcfafaaf2b7125364040f7340d
shrutijha22 <119048223+shrutijha22@users.noreply.github.com>
2025-03-10 11:08:16 +0530

    prg13.cs
    write a program to show the concept of overloading

---
+++ prg13.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class Calculator
+{
+    // Method to add two integers
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Overloaded method to add three integers
+    public int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    // Overloaded method to add two doubles
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        // Calling different overloaded methods
+        Console.WriteLine(calc.Add(2, 3));          // Output: 5
+        Console.WriteLine(calc.Add(2, 3, 4));       // Output: 9
+        Console.WriteLine(calc.Add(2.5, 3.5));      // Output: 6.0
+    }
+}
